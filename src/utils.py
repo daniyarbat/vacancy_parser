3e8b896f15c4c6a6725814db06bcd8271e212bea
Daniyar Batyrbaev <batyrbaev_daniyar@mail.ru>
2023-10-02 00:51:39 +0600

    ADD: added method for sorting vacancies by salary and name

--- src/utils.py
+++ src/utils.py
@@ -10,4 +10,20 @@ def load_vacancies_from_file(filename: str):
         return []
 
 
+def get_top_n_vacancies(filename: str, n: int):
+    """
+    Сортирует вакансии по значению зарплаты от наивысшей до наименьшей,
+    оставляя только положительные значения зарплаты.
+    """
+    vacancies = load_vacancies_from_file(filename)
+    sorted_vacancies = sorted(vacancies, key=lambda x: x.get('salary_from', 0) or 0, reverse=True)
+    filtered_vacancies = [v for v in sorted_vacancies if v.get('salary_from') is not None and v.get('salary_from') > 0]
+    return filtered_vacancies[:n]
+
+
+def get_sorted_vacancies(filename: str):
+    """Сортирует вакансии по имени (алфавитный порядок)."""
+    vacancies = load_vacancies_from_file(filename)
+    return sorted(vacancies, key=lambda x: x['name'])
+
 
