7314b520bb7222917bb4155e3a9f486aee767d57
Daniyar Batyrbaev <batyrbaev_daniyar@mail.ru>
2023-10-01 23:46:00 +0600

    ADD: added handler for work with json file

---
+++ src/saver.py
@@ -0,0 +1,33 @@
+import json
+
+
+class JsonSaver:
+    """Класс для сохранения информации о вакансиях в JSON-файл."""
+
+    def __init__(self, filename: str):
+        self.filename = filename
+
+    def load_existing_data(self):
+        try:
+            with open(self.filename, 'r', encoding='utf-8') as file:
+                return json.load(file)
+        except FileNotFoundError:
+            return []
+
+    def write_vacancies(self, vacancies):
+        existing_data = self.load_existing_data()
+
+        new_data = []
+        for vacancy in vacancies:
+            new_data.append({
+                'name': vacancy.name,
+                'url': vacancy.url,
+                'description': vacancy.description,
+                'salary_to': vacancy.salary_to,
+                'salary_from': vacancy.salary_from
+            })
+
+        combined_data = existing_data + new_data
+
+        with open(self.filename, 'w', encoding='utf-8') as file:
+            json.dump(combined_data, file, ensure_ascii=False, indent=4)
